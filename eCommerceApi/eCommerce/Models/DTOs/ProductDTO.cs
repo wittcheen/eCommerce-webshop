2c9e0d68bcd392222285101308f2a36879966585
Wittchen <wittchenchris@gmail.com>
2026-04-16 11:18:03 +0200

    Fixed a typo

--- eCommerceApi/eCommerce/Models/DTOs/ProductDTO.cs
+++ eCommerceApi/eCommerce/Models/DTOs/ProductDTO.cs
@@ -6,7 +6,7 @@ namespace eCommerce.Models.DTOs
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public int Quantity { get; set; }
+        public int Stock { get; set; }
         public int CategoryID { get; set; }
     }
 
--- eCommerceApi/eCommerce/Models/Product.cs
+++ eCommerceApi/eCommerce/Models/Product.cs
@@ -11,7 +11,7 @@ namespace eCommerce.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public int Quantity { get; set; }
+        public int Stock { get; set; }
 
         [ForeignKey("CategoryID")]
         public int CategoryID { get; set; }
