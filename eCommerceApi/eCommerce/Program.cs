6243fa489ce3ffdfe8b705ffcb8d29fe92262f7a
Wittchen <wittchenchris@gmail.com>
2026-04-18 17:19:21 +0200

    Fixed swagger auth

--- eCommerceApi/eCommerce/Program.cs
+++ eCommerceApi/eCommerce/Program.cs
@@ -6,6 +6,7 @@ using eCommerce.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace eCommerce
 {
@@ -47,7 +48,28 @@ namespace eCommerce
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            #region AI Generated
+            builder.Services.AddSwaggerGen(options =>
+            {
+                OpenApiSecurityScheme jwtSecurityScheme = new()
+                {
+                    Type = SecuritySchemeType.Http,
+                    Scheme = JwtBearerDefaults.AuthenticationScheme,
+
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+
+                options.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    { jwtSecurityScheme, Array.Empty<string>() }
+                });
+            });
+            #endregion
 
             var app = builder.Build();
 
