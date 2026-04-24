219ba2483b5bdb3cc0341cae59672607c16f85ff
Wittchen <wittchenchris@gmail.com>
2026-04-24 13:39:41 +0200

    Added checkout route

--- eCommerceApp/src/router/index.js
+++ eCommerceApp/src/router/index.js
@@ -4,6 +4,10 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
+            path: "/checkout",
+            component: () => import("@/views/Checkout.vue")
+        },
+        {
             path: "/",
             component: () => import("@/layout/layout.vue"),
             children: [
