30e72b8f77f89f2bcf17b39c2720c3e599506b54
Wittchen <wittchenchris@gmail.com>
2026-04-27 14:49:22 +0200

    Created a form field component

---
+++ eCommerceApp/src/components/forms/form-field.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { Message } from "primevue";
+
+defineProps({
+    form: {
+        type: Object, required: true
+    },
+    name: {
+        type: String, required: true
+    },
+    label: {
+        type: String, required: true
+    },
+    required: {
+        type: Boolean, default: false
+    }
+});
+</script>
+
+<template>
+    <label>
+        <span class="inline-block mb-1">{{ label }}</span>
+        <span v-if="required" class="pl-1 text-sm select-none">*</span>
+        <slot :class="'rounded-sm py-1 focus:border-surface-800'" />
+        <Message class="min-h-5.25 whitespace-pre-line" severity="error" variant="simple" size="small">{{ form?.[name]?.error?.message }}</Message>
+    </label>
+</template>
+
+<style scoped></style>
