de540b4f893a73fbc8cbeb37f1fdfb18401ce1a9
Wittchen <wittchenchris@gmail.com>
2026-04-27 18:42:31 +0200

    Minor fix

--- eCommerceApp/src/layout/layout.vue
+++ eCommerceApp/src/layout/layout.vue
@@ -9,7 +9,7 @@ const drawerVisible = ref(false);
 <template>
     <Topbar @open-drawer="drawerVisible = true" />
     <CartDrawer v-model:visible="drawerVisible" />
-    <div class="pt-24 md:pt-[5.5rem] px-8 flex flex-col min-h-screen">
+    <div class="pt-[5.5rem] px-8 flex flex-col min-h-screen">
         <main class="pb-8 2xl:px-16 flex-1">
             <router-view />
         </main>
