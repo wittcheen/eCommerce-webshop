7c7c5a0c89528f310043de88781fc64a3c85dcce
Wittchen <wittchenchris@gmail.com>
2026-04-30 22:11:16 +0200

    Changed theme colors

--- eCommerceApp/src/assets/theme.js
+++ eCommerceApp/src/assets/theme.js
@@ -4,17 +4,17 @@ import Aura from '@primeuix/themes/aura';
 export default definePreset(Aura, {
     semantic: {
         primary: {
-            50: "{amber.50}",
-            100: "{amber.100}",
-            200: "{amber.200}",
-            300: "{amber.300}",
-            400: "{amber.400}",
-            500: "{amber.500}",
-            600: "{amber.600}",
-            700: "{amber.700}",
-            800: "{amber.800}",
-            900: "{amber.900}",
-            950: "{amber.950}"
+            50: "{slate.50}",
+            100: "{slate.100}",
+            200: "{slate.200}",
+            300: "{slate.300}",
+            400: "{slate.400}",
+            500: "{slate.500}",
+            600: "{slate.600}",
+            700: "{slate.700}",
+            800: "{slate.800}",
+            900: "{slate.900}",
+            950: "{slate.950}"
         },
         colorScheme: {
             light: {
